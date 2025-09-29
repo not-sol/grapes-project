56992bb5d8a53befc9f90f37eff27c7627eca377
not-sol <castones.sol@gmail.com>
2025-09-30 01:47:49 +0800

    fix: api fix

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const express = require('serverless-express/express');
+const express = require('@vendia/serverless-express');
 const app = require('./src/app');
 
 exports.handler = express({ app });
